d61303143ee6e75df14d25dc1f5b7d5a809f492f
QBeukelman <quentin.nextresearch@gmail.com>
2025-06-30 17:54:32 +0200

    #34/ Finished M07

--- Module_07/ex02/include/Array.hpp
+++ Module_07/ex02/include/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: quentinbeukelman <quentinbeukelman@stud      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/28 14:40:56 by quentinbeuk   #+#    #+#                 */
-/*   Updated: 2025/06/28 15:12:12 by quentinbeuk   ########   odam.nl         */
+/*   Updated: 2025/06/30 16:32:08 by quentinbeuk   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,17 @@
 template <class T> class Array {
 	private:
 		T*				data;
-		unsigned int	size;
+		unsigned int	arraySize;
 
 	public:
 		Array();
 		Array(unsigned int n);
 		Array(const Array& other);
-		Array operator=(const Array& other);
+		Array& operator=(const Array& other);
 		~Array();
+
+		T& operator[](unsigned int index);
+		const T& operator[](unsigned int index) const;
 
 		unsigned int	size(void) const;
 
--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -6,15 +6,142 @@
 /*   By: quentinbeukelman <quentinbeukelman@stud      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/28 15:12:39 by quentinbeuk   #+#    #+#                 */
-/*   Updated: 2025/06/28 15:13:17 by quentinbeuk   ########   odam.nl         */
+/*   Updated: 2025/06/30 17:52:21 by quentinbeuk   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	Tip: Try to compile int * a = new int(); then display *a.
 */
+#include <iostream>
 #include "include/Array.hpp"
+#include "include/writer.hpp"
+#include "sources/Array.tpp"
 
-int	main(void) {
+#define MAX_VAL 10
+
+int main(int, char**)
+{
+	Array<int>	numbers(MAX_VAL);
+	int*		mirror = new int[MAX_VAL];
 	
+	srand(time(NULL));
+
+	write_divider();
+	write_title("TEST 0: Default Constructor (empty array)");
+	{
+		Array<int>	empty;
+
+		if (empty.size() == 0) {
+			std::cout
+				<< C_BLUE
+				<< "PASSES: Array size is: "
+				<< empty.size()
+				<< RESET_COLOR
+				<< std::endl;
+		} else {
+			std::cerr
+				<< C_RED
+				<< "FAILED: Array size is: "
+				<< empty.size()
+				<< ", but should be 0."
+				<< RESET_COLOR
+				<< std::endl;
+		}
+
+		try {
+			empty[0] = 42;
+			std::cerr
+				<< C_RED
+				<< "FAILED: No exception was thrown when accessing empty array."
+				<< RESET_COLOR
+				<< std::endl;
+		} catch (std::exception& e) {
+			std::cout
+				<< C_BLUE
+				<< "PASSES: Exception caught when accessing element in empty array: \n"
+				<< C_RED
+				<< e.what()
+				<< RESET_COLOR
+				<< std::endl;
+		}
+	}
+
+	write_divider();
+	write_title("TEST 1: Create & Match Arrays");
+	{
+		for (int i = 0; i < MAX_VAL; i++) {
+			const int value = rand();
+	
+			numbers[i] = value;
+			mirror[i] = value;
+		}
+	
+		for (int i = 0; i < MAX_VAL; i++) {
+			if (mirror[i] == numbers[i]) {
+				std::cout
+					<< "Numbers matched mirror: "
+					<< C_BLUE
+					<< numbers[i]
+					<< RESET_COLOR
+					<< std::endl;
+			} else {
+				std::cerr
+					<< C_RED
+					<< "Didn't save the same value!"
+					<< RESET_COLOR
+					<< std::endl;
+				return (1);
+			}
+		}
+	}
+
+	write_divider();
+	write_title("TEST 2: Test Copy/Assignment Constructors");
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	
+		tmp[0] = 999;
+		test[1] = 888;
+
+		if (numbers[0] == 999 || numbers[1] == 888) {
+			std::cerr
+				<< C_RED
+				<< "FAILED: original array was modified."
+				<< RESET_COLOR
+				<< std::endl;
+		} else {
+			std::cout
+				<< C_BLUE
+				<< "PASSED: original array is intact."
+				<< RESET_COLOR
+				<< std::endl;
+		}
+	}
+	
+	write_divider();
+	write_title("TEST 3: Out Of Bounds Exception");
+	{
+		try {
+			numbers[-2] = 0;
+		} catch(const std::exception& e) {
+			std::cerr
+				<< C_RED
+				<< e.what()
+				<< RESET_COLOR << '\n';
+		}
+	
+		try {
+			numbers[MAX_VAL] = 0;
+		} catch(const std::exception& e) {
+				std::cerr
+				<< C_RED
+				<< e.what()
+				<< RESET_COLOR << '\n';
+		}
+	}
+	write_divider();
+
+	delete [] mirror;
 }
